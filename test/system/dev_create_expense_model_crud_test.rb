272f5efef8f04ff3143ad5a724d43cfc718ea771
jose uribe <jse.uribe@gmail.com>
2026-08-19 18:47:43 -0500

    test: QA failed - Dev: Create Expense Model & CRUD

--- test/system/dev_create_expense_model_crud_test.rb
+++ test/system/dev_create_expense_model_crud_test.rb
@@ -1,15 +1,53 @@
 require "application_system_test_case"
 
 class DevCreateExpenseModelCrudTest < ApplicationSystemTestCase
-  test "expense model and crud exist" do
-    # Verify expense model exists
-    assert defined?(Expense), "Expense model should be defined"
-    
-    # Verify controller exists
-    assert defined?(ExpensesController), "ExpensesController should be defined"
-    
-    # Verify routes exist
-    assert Rails.application.routes.recognize_path("/expenses", method: :get)
-    assert Rails.application.routes.recognize_path("/expenses/new", method: :get)
+  setup do
+    # Any setup needed before each test
   end
+  
+  class ExpenseCrudTest < ApplicationSystemTestCase
+  test "user can create an expense" do
+    visit "/expenses/new"
+    fill_in "Amount", with: "50.00"
+    select "Food", from: "Category"
+    fill_in "Date", with: "2023-08-01"
+    fill_in "Description", with: "Lunch at cafe"
+    click_button "Create Expense"
+    assert_text "Expense was successfully created."
+    assert_current_path "/expenses"
+  end
+
+  test "user can view an expense" do
+    visit "/expenses"
+    click_link "Show", match: :first
+    assert_text "Lunch at cafe"
+    assert_text "50.00"
+    assert_text "Food"
+    assert_text "2023-08-01"
+    assert_current_path %r{/expenses/\d+}
+  end
+
+  test "user can update an expense" do
+    visit "/expenses"
+    click_link "Edit", match: :first
+    fill_in "Amount", with: "60.00"
+    select "Travel", from: "Category"
+    fill_in "Description", with: "Taxi"
+    click_button "Update Expense"
+    assert_text "Expense was successfully updated."
+    assert_text "60.00"
+    assert_text "Travel"
+    assert_text "Taxi"
+    assert_current_path %r{/expenses/\d+}
+  end
+
+  test "user can delete an expense" do
+    visit "/expenses"
+    click_link "Delete", match: :first
+    page.driver.browser.switch_to.alert.accept
+    assert_text "Expense was successfully destroyed."
+    assert_current_path "/expenses"
+    assert_no_selector "td", text: "Lunch at cafe"
+  end
+end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,3 +18,7 @@ end
 
 require "capybara/rails"
 require "capybara/minitest"
+
+
+require "capybara/rails"
+require "capybara/minitest"
