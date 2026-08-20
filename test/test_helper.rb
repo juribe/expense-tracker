7c42ac75e3edeb357f29139a15dee4906324b47e
jose uribe <jse.uribe@gmail.com>
2026-08-19 22:34:28 -0500

    qa: Review: Expense Form Design approved

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require "rails/test_help"
 
 module ActiveSupport
   class TestCase
-    parallelize(threads: [Etc.nprocessors, 4].min)
+    parallelize(workers: :number_of_processors)
 
     fixtures :all
 
