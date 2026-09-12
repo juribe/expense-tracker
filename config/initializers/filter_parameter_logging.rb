2df1164191dc956f1b75373502e6db961a63377b
juribe <jse.uribe@gmail.com>
2026-09-12 11:40:43 -0500

    add evaluation dummy html in expense playground

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,8 @@
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
+  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc,
+  # Raw base64 payloads (file/image/audio uploads) are in-memory only; never
+  # let Rails echo them into the request log.
+  :file_data, :image_data, :audio_data
 ]
